e48d78ff74cf3d5fc66a41d669a9f307a3361b9a
juan97padilla <juanda_padiroch@hotmail.com>
2024-12-19 23:15:47 -0700

    avance a video 10 seccion 2

--- src/intro/03-classes.ts
+++ src/intro/03-classes.ts
@@ -1,11 +1,38 @@
-
+import axios from 'axios';
 
 export class Pokemon {
 
+    get imageUrl(): string {
+        return `https://pokemon.com/${ this.id }.jpg `;
+    }
+
     constructor(
-        public id: number, 
+        public readonly id: number, 
         public name: string
     ) {}
+
+    scream() {
+        console.log(`${this.name.toUpperCase()}!!!`);
+    }
+
+    speak() {
+        console.log(`${this.name}, ${this.name}`);
+    }
+
+    async getMoves() {
+        const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon/4');
+        console.log(data.moves);
+
+        return data.moves;
+    }
 }
 
-export const charmander = new Pokemon(4, 'Charmander')
+
+export const charmander = new Pokemon(4, 'Charmander');
+
+// console.log(charmander.imageUrl);
+// charmander.scream();
+// charmander.speak();
+
+// console.log(charmander.getMoves());
+charmander.getMoves();
